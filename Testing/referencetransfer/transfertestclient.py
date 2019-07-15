649193fc642f510d22442148d4d8d75816997b0c
Stephen Yang <stepheny755@gmail.com>
2019-07-15 12:55:47 +0800

    i dont think this works

--- Testing/referencetransfer/transfertestclient.py
+++ Testing/referencetransfer/transfertestclient.py
@@ -4,3 +4,5 @@ print("a %d" % transfertestserver.globalval)
 transfertestserver.changeGlobalVal(12345)
 transfertestserver.globalval=12345
 print("b %d" % transfertestserver.globalval)
+
+val = 123412
