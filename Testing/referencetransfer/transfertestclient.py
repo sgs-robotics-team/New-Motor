e9eee22e3b5c0a6cbc0916230bb5e8adf7debab0
Stephen Yang <stepheny755@gmail.com>
2019-07-15 16:30:11 +0800

    messing around
    with transfer test, which still doesn't work. but thats ok

--- Testing/referencetransfer/transfertestclient.py
+++ Testing/referencetransfer/transfertestclient.py
@@ -1,8 +1,9 @@
 import transfertestserver
+from __main__ import *
 
 print("a %d" % transfertestserver.globalval)
-transfertestserver.changeGlobalVal(12345)
-transfertestserver.globalval=12345
+#transfertestserver.changeGlobalVal(12345)
+#transfertestserver.globalval=12345
 print("b %d" % transfertestserver.globalval)
 
 val = 123412
