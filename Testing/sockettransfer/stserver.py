5103cac753c0818121da1758e6c0a71c250cf5d3
Stephen Yang <stepheny755@gmail.com>
2019-07-16 14:24:15 -0700

    example implementation of server/client over localhost

--- Testing/sockettransfer/stserver.py
+++ Testing/sockettransfer/stserver.py
@@ -4,14 +4,19 @@ import threading,os
 HOST = '127.0.0.1'
 PORT = 50008
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    s.bind((HOST,PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by',addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+while True:
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        i=0
+        s.bind((HOST,PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by',addr)
+            while True:
+                print(i)
+                i+=1;
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                print(data)
