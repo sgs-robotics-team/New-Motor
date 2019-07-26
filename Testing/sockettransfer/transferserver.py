f83c62e8de45f48c6835b362b62ca82cce9558fc
Stephen Yang <stepheny755@gmail.com>
2019-07-26 01:00:07 -0700

    Update transferserver.py
    updated the wrong script :( yikes

--- Testing/sockettransfer/transferserver.py
+++ Testing/sockettransfer/transferserver.py
@@ -1,7 +1,5 @@
-import socket
-import threading
-import os
-import selectors
+import seadrone.smart_thruster as thrusters
+import socket,threading,os,selectors,time,math
 
 HOST = '127.0.0.2'
 PORT = 50008
@@ -11,30 +9,41 @@ BUFSIZE = HEADERSIZE+MESSAGESIZE
 RECV = 125
 SEND = 255
 
+N_motors = 8
+port = '/dev/ttyS3'
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    i=0
-    s.bind((HOST,PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by',addr)
-        while True:
-            print(i)
-            i+=1;
-            data = conn.recv(BUFSIZE)
-            #conn.sendall(b'testingtesting')
-            if not data:
-                break
-            print(data)
-            print(list(data))
-            #print(list(data[4:]))
-            if(list(data)[0]==RECV):
-                s = "Hi, server says hi after receiving from client"
-                print("sending: %s" % s)
-                conn.sendall(s.encode())
-            elif(list(data)[0]==SEND):
-                print("received:")
-                print(data[4:])
-                #do stuff with motor commands here
-            #print(data.decode("ascii"))
+#print("Starting Thrusters...")
+#m = thrusters.start(N_motors,port)
+
+def main_thread():
+    while True: #replace with while(m.running):
+        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+            i=0
+            s.bind((HOST,PORT))
+            s.listen(1)
+            conn, addr = s.accept()
+            with conn:
+                print('Connected by',addr)
+                while True:
+                    print(i)
+                    i+=1;
+                    data = conn.recv(BUFSIZE)
+                    #conn.sendall(b'testingtesting')
+                    if not data:
+                        break
+                    print(data)
+                    print(list(data))
+                    #print(list(data[4:]))
+                    if(list(data)[0]==RECV):
+                        s = "Hi, server says hi after receiving from client"
+                        print("sending: %s" % s)
+                        conn.sendall(s.encode())
+                    elif(list(data)[0]==SEND):
+                        print("received:")
+                        print(data[4:])
+                        #do stuff with motor commands here
+                    #print(data.decode("ascii"))
+
+if(__name__=="__main__"):
+    thread = threading.Thread(target=main_thread)
+    thread.start()
