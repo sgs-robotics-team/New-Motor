481fa35aea73bce9348a70a7cb777389bfa0eac4
Stephen Yang <stepheny755@gmail.com>
2019-07-29 15:37:06 -0700

    Update transferserver.py

--- Debug/transferserver.py
+++ Debug/transferserver.py
@@ -8,31 +8,45 @@ MESSAGESIZE = 8
 BUFSIZE = HEADERSIZE+MESSAGESIZE
 RECV = 125
 SEND = 255
-
 N_motors = 8
 port = '/dev/ttyS3'
 
-MAX_RPM = 5000
-EFC_RPM = 0.75*MAX_RPM
+MAX_RPM = 5000 #absolute maximum rpm of thrusters
+EFC_RPM = 0.75*MAX_RPM #preferably dont use all of the RPM :)
 
-#print("Starting Thrusters...")
-#m = thrusters.start(N_motors,port)
+zeroarr = [0 for x in range(N_motors)]
 
-def main_thread():
+print("Starting Thrusters...")
+m = thrusters.start(N_motors,port)
 
+def updaterpms(m,target):
+    for i in range(N_motors):
+        m.target_rpm[i]=target[i]
+
+if(__name__=="__main__"):
+    print("Python Thread Started")
     targetrpms=[0 for x in range(N_motors)]
-
-    while True: #replace with while(m.running):
+    while m.running:
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-            i=0
             s.bind((HOST,PORT))
             s.listen(1)
             conn, addr = s.accept()
+            for id in m.motors:
+                m.target_rpm[id]=250#targetrpms[id]
+                motor_feedback = 'Motor {:2d}: '.format(id)
+                motor_feedback += "ON  " if m.is_on[id] else "OFF "
+                motor_feedback += '{:5d}rpm '.format(m.rpm[id])
+                motor_feedback += '{:5.2f}A '.format(m.current[id])
+                motor_feedback += '{:5.2f}V '.format(m.voltage[id])
+                motor_feedback += '{:5.2f}C '.format(m.driver_temperature[id])
+                motor_feedback += 'Alarm: ' + m.get_alarm_description(id)
+                print(motor_feedback)
+                if m.has_alarm[id]:
+                    print("Auto-resetting motor alarm")
+                    m.reset_alarm(id)
             with conn:
                 print('Connected by',addr)
                 while True:
-                    print(i)
-                    i+=1;
                     data = conn.recv(BUFSIZE)
                     #conn.sendall(b'testingtesting')
                     if not data:
@@ -50,6 +64,5 @@ def main_thread():
                         #do stuff with motor commands here
                     #print(data.decode("ascii"))
 
-if(__name__=="__main__"):
-    thread = threading.Thread(target=main_thread)
-    thread.start()
+updaterpms(m,zeroarr)
+m.stop()
