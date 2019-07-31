541a735a599b7efc0ec97ed7d972194728934354
Stephen Yang <stepheny755@gmail.com>
2019-07-30 18:46:27 -0700

    updated motor class

--- Debug/motor.cpp
+++ Debug/motor.cpp
@@ -1,7 +1,47 @@
-
+#include <cstdlib>
+#include <cstdio>
 
 #include "motor.h"
 #include "serialize.h"
 #include "transferclient.h"
 
 #define NUMMOTORS 8
+
+motor::motor(): tco(),so((int)NUMMOTORS){//(transferclient a,serialize b){
+  this->targetrpms = (int*) malloc(NUMMOTORS*sizeof(int));
+  this->currentrpms = (int*) malloc(NUMMOTORS*sizeof(int));
+  initialize();
+  printf("Motor: RPM Arrays Initialized\n");
+  test();
+}
+
+void motor::initialize(){
+  for(int i = 0;i < NUMMOTORS;i++){
+    targetrpms[i]=0;
+    currentrpms[i]=0;
+  }
+}
+
+int motor::setTargetRPM(int mID,int val){
+
+  targetrpms[mID]=val;
+
+}
+
+int motor::getTargetRPM(int mID){
+  //updatecurrentrpms;
+  return currentrpms[mID];
+}
+
+void motor::test(){
+  printf("\nMotor class functional\n");
+  tco.test();
+  so.test();
+}
+
+motor::~motor(){
+  printf("Motor: Shutting Down...\n");
+  free(targetrpms);
+  free(currentrpms);
+  printf("Motor: Integer Arrays freed.")
+}
--- Debug/motor.h
+++ Debug/motor.h
@@ -1,3 +1,6 @@
+#include "serialize.h"
+#include "transferclient.h"
+
 #ifndef MOTOR_H
 #define MOTOR_H
 
@@ -8,9 +11,26 @@ public:
   motor();
   ~motor();
 
+  void initialize();
+
+  int setTargetRPM(int mID,int val);
+  int* setTargetRPM(int val);
+
+  int getTargetRPM(int mID);
+  int* getTargetRPM();
+
+  int updateCurrentRPMs();//recv from python thread
+  int updateTargetRPMs();//send to python thread
+
+  void test();
+
 private:
-  char* rpmbuffer;
-  
+
+  int* targetrpms;
+  int* currentrpms;
+
+  transferclient tco;
+  serialize so;
 };
 
 #endif
