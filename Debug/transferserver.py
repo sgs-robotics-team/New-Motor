8f9c40d34b201973c53e1952355ae08a5432469c
Stephen Yang <stepheny755@gmail.com>
2019-08-08 14:34:32 -0700

    Update transferserver.py

--- Debug/transferserver.py
+++ Debug/transferserver.py
@@ -22,6 +22,12 @@ class Data:
         self.current_rpm=[0 for x in range(N_motors)]
         self.data_string=""
 
+    def get_tRPMs(self):
+        return self.target_rpm
+
+    def get_cRPMs(self):
+        return self.current_rpm
+
     def set_tRPMs(self,id,val):
         self.target_rpm[i]=val
 
@@ -36,6 +42,8 @@ def feedback_thread(m,d):
         feedback = ""
         for id in m.motors:
             d.current_rpm[id] = m.rpm[id]
+            if(m.target_rpm[id]!=d.target_rpm[id]):
+                m.target_rpm[id]=d.target_rpm[id]
             feedback += "M{:2d}".format(id)
             feedback += "Y" if m.is_on[id] else "N"
             feedback += '{:5d}R'.format(m.rpm[id])
@@ -50,11 +58,6 @@ def feedback_thread(m,d):
         d.set_str(feedback)
         time.sleep(0.05)
 
-def command_thread(m,d):
-    while m.running:
-
-        time.sleep(0.01)
-
 print("Starting Thrusters...")
 m = thrusters.start(N_motors,port)
 d = Data()
@@ -65,22 +68,17 @@ if(__name__=="__main__"):
     t1 = threading.Thread(target=feedback_thread, args=(m,d))
     t1.daemon = True
     t1.start()
-    print("Command Thread Started")
-    t2 = threading.Thread(target=command_thread, args=(m,d))
-    t2.daemon = True
-    t2.start()
+
     print("Main/Server Thread Started")
-    #print(m.running)
     while m.running:
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
             s.bind((HOST,PORT))
-            #s.listen(1)
-            #conn, addr = s.accept()
+            s.listen(1)
+            conn, addr = s.accept()
             with conn:
                 print('Connected by',addr)
                 while True:
                     data = conn.recv(BUFSIZE)
-                    #conn.sendall(b'testingtesting')
                     if not data:
                         break
                     print(data)
@@ -90,11 +88,10 @@ if(__name__=="__main__"):
                         s = d.data_string
                         conn.sendall(s.encode())
                     elif(list(data)[0]==SEND):
+                        print(data[HEADERSIZE:])
                         for id in m.motors:
-                            if(m.target_rpm[id]!=d.target_rpm[id]):
-                                m.target_rpm[id]=d.target_rpm[id]
-                        print(data[4:])
-                        #do stuff with motor commands here
+                            d.set_tRPMs(id,data[id+HEADERSIZE])
+                        print(d.get_tRPMs())
                     #print(data.decode("ascii"))
 
 updaterpms(m,zeroarr)
