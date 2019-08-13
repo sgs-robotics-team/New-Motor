1fbfb91a12082b0005f4f43916849670583e0b4e
Stephen Yang <stepheny755@gmail.com>
2019-08-13 14:46:22 -0700

    Update main.cpp

--- Debug/main.cpp
+++ Debug/main.cpp
@@ -20,27 +20,9 @@ int main(){
   printf("\n---START---\n");
 
 
-  uint16_t test = 4598;
-  printf("%d\n",test);
-  unsigned char a = (unsigned char)(test>>8);
-  unsigned char b = (unsigned char)(test & 0xff);
-  printf("%d\n",(test>>8));
-  char* cbuf = (char*) malloc(2);
-  cbuf[0]=a;
-  cbuf[1]=b;
-  for(int i = 0;i<strlen(cbuf);i++){
-    printf("%d\n",(cbuf[i]));
-  }
-  uint16_t result = (a<<8)+b;
-  printf("%d\n",result);
-  free(cbuf);
-  /*
+  mo.setTargetRPM(500);
+  mo.updateTargetRPMs();
 
-  mo.setTargetRPM(99);
-  for(int i = 0;i < mo.getSize();i++){
-    printf("target: %d current: %d\n",mo.getTargetRPM()[i],mo.getCurrentRPM()[i]);
-  }
-  */
   printf("\n---END---\n\n");
   return 0;
 }
@@ -49,6 +31,13 @@ void loop(){//main loop?
   printf("test");
 }
 
+
+/*
+mo.setTargetRPM(99);
+for(int i = 0;i < mo.getSize();i++){
+  printf("target: %d current: %d\n",mo.getTargetRPM()[i],mo.getCurrentRPM()[i]);
+}
+*/
 
 /*
 transferclient tco;
